9101e2b90c7b8c572c48e16d6c7c2251f5224983
jennaon <yunjung.jenna.lee@gmail.com>
2014-10-14 03:05:53 -0400

    Update LEDColor.cpp

--- LEDColor.cpp
+++ LEDColor.cpp
@@ -1,3 +1,12 @@
+/* First Release April 4, 2013 by Jenna Yun Lee, Cooper Union, All rights reserved.
+I strongly support open source programming. Free to distribute and modify,
+but please keep the original copyright information.
+Contact yunjung.jenna.lee@gmail.com for suggestions/errors/helps!
+
+This code is originally written for a two-player arcade game that makes uses of LED lights.
+Pixel.h and Pixel.cpp allows a programmer to control each pixel from an array of LEDs much easily.
+*/
+
 #include "LedColor.h"
 
 LedColor::LedColor()
